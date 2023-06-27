19480f25e6b54401888bfe3d7f356bfdd1449fc2
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2023-06-27 17:03:08 +0300

    feat: complete task

---
+++ scripts/declinateNum.js
@@ -0,0 +1,6 @@
+function declinateNum(num, wordsArr) {
+  return wordsArr[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+}
+
+export default declinateNum;
+
